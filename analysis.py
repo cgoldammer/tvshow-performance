8fc35683df47d0d798ed1315ea3331ebacaac16e
cgoldammer <goldammer.christian@gmail.com>
2018-04-13 17:14:16 -0400

    Added initial estimation

--- analysis.py
+++ analysis.py
@@ -6,17 +6,66 @@ import imp
 
 import tvshow_functions as f
 
+
+res = f.simulate_show(parameters)
+res.head(20)
+
+
+
+
+
+
+# Right now, there's only a single parameter, the relative standard deviation of noise.
+# in other words, it's the extent to which the outcome of a round is determined by luck
+# or noise.
+
+
+# If the impact of noise if relatively low, then some participants would consistently be at the
+# top or bottom of the rankings. Thus we are using the following metric to estimate how important
+# noise is: "The proportion of times that a non-bottom finisher finishes non-bottom in the next round,
+# relative to random chance"
+
+episodes_remove = ['05', '07', '12.1', '12.2', '13']
+
+df_raw = pd.read_csv("./data/season1.csv").set_index('Participant')
+df_long = df_raw.stack(level=-1).reset_index()
+
+df_long = df_long[df_long.result.notnull()]
+df_long = df_long[~df_long.episode.isin(episodes_remove)]
+df_long.episode.unique()
+
+df_long['top'] = df_long.result.isin(results_top)
+df_long['top_previous'] = df_long.groupby('agent').top.shift(1)
+
+results_top = ['HIGH', 'WIN']
+
+df_long.groupby('top_previous').top.mean()
+
+
+share_top = df_long.groupby('episode').top.mean().mean()
+
+res
+
+
+pers_data = f.persistence(df_long)
+
+rel_lift_data = pers_data[True] / pers_data[False]
+
 number = 10
 std_agent_skill = 1
-std_noise = 1
+relative_std_noise = 2
 
 parameters = {
   'number': number,
   'std_agent_skill': std_agent_skill,
-  'std_noise': std_noise
+  'relative_std_noise': relative_std_noise,
+  'share_top': share_top
 }
 
-f.simulate_show(parameters)
-
-
+# Comparing the median lift in simulated data to the lift observed in the data
+# if they are close, then we have set the parameter correctly.
+rel_lift_data
+parameters
+lifts = [f.relative_lift(parameters) for _ in range(50)]
+np.median(lifts)
 
--- tvshow_functions.py
+++ tvshow_functions.py
@@ -9,11 +9,12 @@ def create_agents(parameters):
 def simulate_round(parameters, agents):
   """Simulate a round and return the distribution of the result"""
   df = pd.DataFrame(agents, columns=['skill'])
-  std_noise = parameters['std_noise']
-  df['noise'] = np.random.normal(0, std_noise)
+  std_noise = parameters['std_agent_skill'] * parameters['relative_std_noise']
+  df['noise'] = np.random.normal(0, std_noise, len(df))
   df['outcome'] = df.skill + df.noise
-
   df['rank'] = df.outcome.rank().astype(int)
+  number = len(agents)
+  df['top'] = df['rank'].div(number) >= 1- parameters['share_top']
 
   return df
 
@@ -23,12 +24,27 @@ def simulate_show(parameters):
 
     results = []
     agents_after_round = agent_skill.index
+    episode = 1
     while len(agents_after_round) > 1:
       result = simulate_round(parameters, agent_skill[agents_after_round])
       lowest_outcome = result.outcome.min()
-      agents_after_round = result[result.outcome > lowest_outcome].index
-      results.append(agents_after_round)
+      results_after_round = result.ix[result.outcome > lowest_outcome, ['skill', 'noise', 'outcome', 'rank', 'top']]
+      results_after_round['episode'] = episode
+      results_after_round['agent'] = results_after_round.index
+      agents_after_round = results_after_round.index
+      results.append(results_after_round)
       len(result)
       len(agents_after_round)
-    return results
+      episode += 1
+    return pd.concat(results).sort_values(['episode', 'agent'])
 
+
+def persistence(df):
+  df['top_previous'] = df.groupby('agent').top.shift(1)
+  return df.groupby('top_previous').top.mean()
+
+
+def relative_lift(parameters):
+  df_sim = simulate_show(parameters)
+  pers_sim = persistence(df_sim)
+  return pers_sim[True] / pers_sim[False]
